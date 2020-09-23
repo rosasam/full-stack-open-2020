b3bbc907bccab787e9a2ac843bbefc18661812b2
Samuel Rosas <samuel.m.rosas@gmail.com>
2020-09-23 14:35:49 +0300

    part 2 exercises 2.15-2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import AddPerson from './components/AddPerson'
 import Search from './components/Search'
-import axios from 'axios'
-
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchQuery, setSearchQuery ] = useState('')
@@ -14,13 +13,14 @@ const App = () => {
   const shownPersons = persons.filter(
     person => person.name.toLowerCase().includes(searchQuery.toLowerCase()))
 
-  // Fetch Persons from JSON database
-  useEffect(() => axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-    }), [])
+  // GET persons from JSON database
+  useEffect(() => {
+    personService
+    .getAll()
+    .then(persons => setPersons(persons))
+  }, [])
 
+  // Handle input fields
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -33,21 +33,73 @@ const App = () => {
     setSearchQuery(event.target.value)
   }
 
+  // CREATE person
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.map(p => p.name).includes(newName)) {
-      window.alert(`${newName} is already added to phonebook`)
-      return;
+
+    // Attempt to update number if the name is already in the phonebook
+    const existingPerson = persons.find(p => p.name === newName)
+    if (existingPerson) {
+      updateNumber(existingPerson.id, newNumber)
+      // Intentionally return before "wiping" the input fields.
+      // This feels like the most natural beahviour after clicking cancel.
+      return
     }
 
     const newPerson = {
       name: newName,
       number: newNumber,
     }
+    // The returned person object also contains the id created on the server
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+      })
 
-    setPersons(persons.concat(newPerson))
     setNewName('')
     setNewNumber('')
+  }
+
+  // UPDATE person
+  const updateNumber = (id, newNumber) => {
+    const person = persons.find(p => p.id === id)
+    if (!window.confirm(
+      `${person.name} is already added to the phonebook,` +
+      `replace the old number with a new one?`)) {
+      return
+    }
+
+    const newPerson = {...person, number: newNumber}
+    personService
+      .update(id, newPerson)
+      .then(updatedPerson => setPersons(persons.map(
+        p => p.id !== id ? p : updatedPerson)))
+      .catch(error => handleServerError(error, person))
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+
+  // DELETE person
+  const deletePerson = id => {
+    const person = persons.find(p => p.id === id)
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .delete_(id)
+        .then(setPersons(persons.filter(p => p.id !== id)))
+        .catch(error => handleServerError(error, person))
+    }  
+  }
+
+  // Handles server errors "gracefully" (debatable if alert is graceful),
+  // assuming that errors stem from trying to interact with an already deleted object
+  const handleServerError = (error, person) => {
+    alert(
+      `${person.name} was already deleted from the server.`
+    )
+    setPersons(persons.filter(p => p.id !== person.id))
   }
 
   return (
@@ -74,6 +126,7 @@ const App = () => {
         <h2>Numbers</h2>
         <Persons
           persons={shownPersons}
+          deleteHandler={deletePerson}
         />
       </div>
     </div>
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
-const Persons = ({persons}) => {
+const Persons = ({ persons, deleteHandler }) => {
+  return (
+    <ul>
+      {persons.map(person => 
+        <Person 
+          key={person.id}
+          person={person}
+          deleteHandler={() => deleteHandler(person.id)}
+        />
+      )}
+    </ul>
+  )
+}
+
+const Person = ({ person, deleteHandler }) => {
     return (
-        <ul>
-            {persons.map(person => 
-            <li key={person.name}>{person.name} {person.number}</li>
-            )}
-        </ul>
-    )
+      <li>
+        {person.name} {person.number} 
+        <button onClick={deleteHandler}>delete</button>
+      </li>)
 }
 
 export default Persons
