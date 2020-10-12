73b84cbdba71608499409c4894440d8dc8a93882
Samuel Rosas <samuel.m.rosas@gmail.com>
2020-10-12 16:49:34 +0300

    part4, exercises 4.1-4.2: add middleware and input validation for POST

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 
 logger.info(`Connecting to ${config.MONGODB_URI}`)
@@ -27,7 +28,11 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 // bind blogsRouter to /api/notes endpoint
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ part4/utils/middleware.js
@@ -0,0 +1,20 @@
+const logger = require('./logger')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'ValidationError') {
+    response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler
+}
