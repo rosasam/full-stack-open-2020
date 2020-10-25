e4ee4a93f3119c77c580fda56f7e28354632a106
Samuel Rosas <samuel.m.rosas@gmail.com>
2020-10-25 10:30:10 +0200

    Refactor part4 task

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -6,7 +6,7 @@ let MONGODB_URI = process.env.MONGODB_URI
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
-
+console.log('uri', MONGODB_URI)
 module.exports = {
   PORT,
   MONGODB_URI
