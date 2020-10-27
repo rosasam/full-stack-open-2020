f4349a0669713fa540d3f441e9b456ba349b1f56
Samuel Rosas <samuel.m.rosas@gmail.com>
2020-10-27 22:00:11 +0200

    work on exercise 4.13

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+  const blog = new Blog({
+    ...body,
+    likes: body.likes ? body.likes : 0
+  })
+  const result = await blog.save()
+  response.status(201).json(result)
+})
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch((error) => next(error))
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,43 +1,117 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    author: 'Wei Shi Lindon',
-    title: 'The Path of Twin Stars',
-    url: 'google.com',
-    likes: 3
-  },
-  {
-    author: 'Yerin',
-    title: 'The Path of The Endless Sword',
-    url: 'google.com',
-    likes: 2
-  },
-  {
-    author: 'Eithan Arelius',
-    title: 'Unknown Pure Madra Path',
-    url: 'google.com',
-    likes: 512
-  },
-]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  for (let blog of initialBlogs)
+  for (let blog of helper.initialBlogs) {
     const blogObject = new Blog(blog)
-    blogObject.save()
+    await blogObject.save()
+  }
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there are initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('Go To Statement Considered Harmful')
+  })
+  
+  test('the unique identifier property of blogs is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
+})
+
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
+    const blogTitle = 'Path of Twin Stars'
+    const newBlog = {
+      'title': blogTitle,
+      'author': 'Wei Shi Lindon',
+      'url': 'https://www.willwight.com/a-blog-of-dubious-intent',
+      'likes': 50426
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+  
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain(blogTitle)
+  })
+  
+  test('succeeds without likes in the request', async () => {
+    const blogTitle = 'Path of The Hollow King'
+    const newBlog = {
+      'title': blogTitle,
+      'author': 'Eithan Arelius',
+      'url': 'https://www.willwight.com/a-blog-of-dubious-intent',
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(response.body.pop().likes).toEqual(0)
+  })
+  
+  test('fails when title and url are missing', async () => {
+    // TODO: Should test each missing field separately
+    const blogTitle = 'Path of The Hollow King'
+    const newBlog = {
+      'author': 'Eithan Arelius',
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+})
+
+
+describe('deletion of a blog', () => {
+  test('succeeds if id is valid', async () => {
+    const blogsBefore = await helper.blogsInDb()
+    const blogToDelete = blogsBefore[0]
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+    
+    const blogsAfter = await helper.blogsInDb()
+    expect(blogsAfter).toHaveLength(helper.initialBlogs.length - 1) 
+  })
 })
 
 afterAll(() => {
